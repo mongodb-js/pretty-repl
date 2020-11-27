720eef1b038917c0f26d30bba6133f3f8dbe601b
Anna Henningsen <anna.henningsen@mongodb.com>
2020-11-27 10:50:31 +0100

    Make module independent of stdio
    Do not hardcode dependencies on stdio streams, when those might
    not actually end up being the input/output streams:
    
    - Pick the right module at REPL creation time based on the options
      passed to the REPL start function, rather than at startup
    - Pass options to chalk that are based on the TTY color support
      (because otherwise chalk would also determine color support at
      startup)
    - Do not use global ansiCursor / colorize variables in the Node.js 12.x
      variant

--- index.js
+++ index.js
@@ -10,5 +10,25 @@ const prettyReplUnsupported = () => require('repl');
 const prettyReplNode12 = () => require('./lib/pretty-repl-compat');
 const prettyRepl = () => require('./lib/pretty-repl');
 
-module.exports = (process.stdout.isTTY !== true || semver.lt(process.version, '11.0.0')) ? prettyReplUnsupported()
+const impl = (tty) => (!tty || semver.lt(process.version, '11.0.0')) ? prettyReplUnsupported()
   : semver.lt(process.version, '13.0.0') ? prettyReplNode12() : prettyRepl();
+
+function isReplTerminal(options) {
+  if (options.terminal !== undefined)
+    return options.terminal;
+  return (options.output || process.stdout).isTTY;
+}
+
+class REPLServer extends (prettyReplUnsupported().REPLServer) {
+  constructor(options = {}) {
+    return new (impl(isReplTerminal(options)).REPLServer)(options);
+  }
+}
+function start(options = {}) {
+  return impl(isReplTerminal(options)).start(options);
+}
+
+module.exports = {
+  REPLServer,
+  start
+};
