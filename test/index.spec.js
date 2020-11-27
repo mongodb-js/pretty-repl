3ef32facfe6fa27994cf605b1d4bea7579d977ce
Anna Henningsen <anna.henningsen@mongodb.com>
2020-11-27 11:19:00 +0100

    Make module independent of stdio (#4)
    Do not hardcode dependencies on stdio streams, when those might
    not actually end up being the input/output streams:
    
    - Pick the right module at REPL creation time based on the options
      passed to the REPL start function, rather than at startup
    - Pass options to chalk that are based on the TTY color support
      (because otherwise chalk would also determine color support at
      startup)
    - Do not use global ansiCursor / colorize variables in the Node.js 12.x
      variant

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 const { stdio } = require('stdio-mock');
 const decache = require('decache');
+const { PassThrough } = require('stream');
 
 const nodeMajorVersion = () => {
   const versionMatcher = process.version.match(/^v(\d{1,2})\.\d{1,2}\.\d{1,2}$/);
@@ -12,12 +13,14 @@ test('loads the right module for the version of node', t => {
   process.stdout.isTTY = true;
   const repl = require('..');
   const major = nodeMajorVersion();
+  const { stdin, stdout } = stdio();
+  const prettyRepl = repl.start({ input: stdin, output: stdout, terminal: true });
   if (major >= 13) {
-    t.ok(repl._moduleLoaded.endsWith('pretty-repl.js'), 'pretty-repl loaded');
+    t.ok(prettyRepl.__prettyModuleLoaded.endsWith('pretty-repl.js'), 'pretty-repl loaded');
   } else if (major < 13 && major >= 11) {
-    t.ok(repl._moduleLoaded.endsWith('pretty-repl-compat.js'), 'pretty-repl-compat (compatibility mode for old node versions) loaded');
+    t.ok(prettyRepl.__prettyModuleLoaded.endsWith('pretty-repl-compat.js'), 'pretty-repl-compat (compatibility mode for old node versions) loaded');
   } else {
-    t.notOk(repl._moduleLoaded, 'pretty-repl not loaded (old version of node)');
+    t.notOk(prettyRepl.__prettyModuleLoaded, 'pretty-repl not loaded (old version of node)');
   }
   decache('..');
 });
@@ -53,7 +56,8 @@ test('applies colors when necesssary', t => {
     prompt: 'test-prompt > ',
     input: stdin,
     output: stdout,
-    colorize: str => str.replace(/const/, '<color>const</color>')
+    colorize: str => str.replace(/const/, '<color>const</color>'),
+    terminal: true
   });
   let out = '';
   stdout.on('data', data => {
@@ -85,6 +89,32 @@ test('does not apply colors when not necesssary', t => {
     out += data;
     if (out.endsWith('\n')) {
       t.equal(out, 'test-prompt > let foo = 12\n', 'output is not colored');
+    }
+  });
+  prettyRepl._writeToOutput('test-prompt > let foo = 12\n');
+  decache('..');
+});
+
+test('picks colors independently of stdio', {
+  skip: nodeMajorVersion() < 12
+}, t => {
+  t.plan(1);
+  process.stdout.isTTY = undefined;
+  const repl = require('..');
+  const { stdin } = stdio();
+  const output = new PassThrough();
+  output.isTTY = true;
+  output.getColorDepth = () => 8;
+  const prettyRepl = repl.start({
+    prompt: 'test-prompt > ',
+    input: stdin,
+    output: output
+  });
+  let out = '';
+  output.setEncoding('utf8').on('data', data => {
+    out += data;
+    if (out.endsWith('\n')) {
+      t.match(out, /(\x1b\[.*m)+let(\x1b\[.*m)+ foo = (\x1b\[.*m)+12(\x1b\[.*m)+/, 'output is colored');
     }
   });
   prettyRepl._writeToOutput('test-prompt > let foo = 12\n');
