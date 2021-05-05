37e4d2d359fa7709a10c5f68e50fc0b7dc3dc523
Anna Henningsen <anna.henningsen@mongodb.com>
2021-05-05 12:31:29 +0200

    Rework internal implementation
    This is intended to be landed as a semver-major change, and drops
    testing for unsupported Node.js versions (10.x, 13.x).
    
    - Remove the legacy, non-class implementation.
    - Remove usage of explicit ANSI cursors.
      - This fixes https://github.com/mongodb-js/pretty-repl/issues/5
        and https://jira.mongodb.org/browse/MONGOSH-734.
    - Make the monkey-patching of `._insertString()` minimal,
      in particular, do not rewrite the full line if we do not have to.
      - This fixes https://jira.mongodb.org/browse/MONGOSH-691.
    - Refactor `._writeOutput()` so that, instead of working on the
      assumption that we’re always re-writing the full line, account
      for the possibility that we need to remove some characters from
      the output and add them back.
      - For example, if the input is `a = functio` and the user types `n`,
        we remove the existing characters for `functio` and instead print
        the newly highlighted `function` keyword, but we do not go back
        further and leave the `a = ` part untouched because its
        highlighting has not changed.
    
    Fixes: https://github.com/mongodb-js/pretty-repl/issues/5
    Fixes: https://jira.mongodb.org/browse/MONGOSH-734
    Fixes: https://jira.mongodb.org/browse/MONGOSH-691

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const semver = require('semver');
+'use strict';
 
 // Functions that return the right implementation
-// depending on the node version.
+// depending on whether we are using a TTY or not.
 // They are functions rather than variables because we
 // only want to execute the require if that's the right version to load.
-// This way, we don't monkey-patch the Interface prototype of the readline
-// module for nothing.
 const prettyReplUnsupported = () => require('repl');
-const prettyReplNode12 = () => require('./lib/pretty-repl-compat');
 const prettyRepl = () => require('./lib/pretty-repl');
 
-const impl = (tty) => (!tty || semver.lt(process.version, '11.0.0')) ? prettyReplUnsupported()
-  : semver.lt(process.version, '13.0.0') ? prettyReplNode12() : prettyRepl();
+const impl = (tty) => !tty ? prettyReplUnsupported() : prettyRepl();
 
 function isReplTerminal(options) {
   if (options.terminal !== undefined)
