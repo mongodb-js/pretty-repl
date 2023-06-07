600f4608fc4b2622d73f8ffaed1c4825a955e603
Maurizio Casimirri <maurizio.cas@gmail.com>
2023-06-07 09:22:23 +0200

    chore: align setup with other repos

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,11 @@
-const test = require('tape');
 const { stdio } = require('stdio-mock');
 const { PassThrough } = require('stream');
+const assert = require('assert');
+
 const repl = require('..');
 const memoizeStringTransformerMethod = require('../lib/memoize-string-transformer');
 
-test('does not apply colors when not TTY', t => {
-  t.plan(1);
+it('does not apply colors when not TTY', (done) => {
   process.stdout.isTTY = undefined;
   const { stdin, stdout } = stdio();
   const prettyRepl = repl.start({
@@ -18,14 +18,14 @@ test('does not apply colors when not TTY', t => {
   stdout.on('data', data => {
     out += data;
     if (out.endsWith('\n')) {
-      t.equal(out, 'test-prompt > const foo = 12\n', 'output is not colored');
+      assert.equal(out, 'test-prompt > const foo = 12\n', 'output is not colored');
+      done();
     }
   });
   prettyRepl._writeToOutput('test-prompt > const foo = 12\n');
 });
 
-test('applies colors when necesssary', t => {
-  t.plan(1);
+it('applies colors when necessary', (done) => {
   process.stdout.isTTY = true;
   const { stdin, stdout } = stdio();
   const prettyRepl = repl.start({
@@ -39,14 +39,14 @@ test('applies colors when necesssary', t => {
   stdout.on('data', data => {
     out += data;
     if (out.endsWith('\n')) {
-      return t.equal(out, 'test-prompt > <color>const</color> foo = 12\n', 'output is colored');
+      assert.equal(out, 'test-prompt > <color>const</color> foo = 12\n', 'output is colored');
+      done();
     }
   });
   prettyRepl._writeToOutput('test-prompt > const foo = 12\n');
 });
 
-test('does not apply colors when not necesssary', t => {
-  t.plan(1);
+it('does not apply colors when not necessary', (done) => {
   process.stdout.isTTY = true;
   const { stdin, stdout } = stdio();
   const prettyRepl = repl.start({
@@ -59,14 +59,14 @@ test('does not apply colors when not necesssary', t => {
   stdout.on('data', data => {
     out += data;
     if (out.endsWith('\n')) {
-      t.equal(out, 'test-prompt > let foo = 12\n', 'output is not colored');
+      assert.equal(out, 'test-prompt > let foo = 12\n', 'output is not colored');
+      done();
     }
   });
   prettyRepl._writeToOutput('test-prompt > let foo = 12\n');
 });
 
-test('picks colors independently of stdio', t => {
-  t.plan(1);
+it('picks colors independently of stdio', (done) => {
   process.stdout.isTTY = undefined;
   const { stdin } = stdio();
   const output = new PassThrough();
@@ -81,30 +81,30 @@ test('picks colors independently of stdio', t => {
   output.setEncoding('utf8').on('data', data => {
     out += data;
     if (out.endsWith('\n')) {
-      t.match(out, /(\x1b\[.*m)+let(\x1b\[.*m)+ foo = (\x1b\[.*m)+12(\x1b\[.*m)+/, 'output is colored');
+      // eslint-disable-next-line no-control-regex
+      assert.match(out, /(\x1b\[.*m)+let(\x1b\[.*m)+ foo = (\x1b\[.*m)+12(\x1b\[.*m)+/, 'output is colored');
+      done();
     }
   });
   prettyRepl._writeToOutput('test-prompt > let foo = 12\n');
 });
 
-test('memoizeStringTransformerMethod', t => {
-  t.plan(6);
+it('memoizeStringTransformerMethod', () => {
   let i = 0;
   const cachedFn = memoizeStringTransformerMethod(3, (str) => {
     // Intentionally use something that depends on external state to test
     // the caching functionality.
     return `${i++}: ${str}`;
   });
-  t.equal(cachedFn('foo'), '0: foo');
-  t.equal(cachedFn('foo'), '0: foo');
-  t.equal(cachedFn('bar'), '1: bar');
-  t.equal(cachedFn('baz'), '2: baz');
-  t.equal(cachedFn('qux'), '3: qux');
-  t.equal(cachedFn('foo'), '4: foo');
+  assert.equal(cachedFn('foo'), '0: foo');
+  assert.equal(cachedFn('foo'), '0: foo');
+  assert.equal(cachedFn('bar'), '1: bar');
+  assert.equal(cachedFn('baz'), '2: baz');
+  assert.equal(cachedFn('qux'), '3: qux');
+  assert.equal(cachedFn('foo'), '4: foo');
 });
 
-test('stripCompleteJSStructures', t => {
-  t.plan(11);
+it('stripCompleteJSStructures', () => {
   const { stdin } = stdio();
   const output = new PassThrough();
   output.isTTY = true;
@@ -113,21 +113,20 @@ test('stripCompleteJSStructures', t => {
     input: stdin,
     output: output
   });
-  t.equal(prettyRepl._stripCompleteJSStructures('{a: (x) => x.y = 1}'), '');
-  t.equal(prettyRepl._stripCompleteJSStructures('{x} `${unfinished'), ' `${unfinished');
-  t.equal(prettyRepl._stripCompleteJSStructures('{(x}'), '{(x}');
-  t.equal(prettyRepl._stripCompleteJSStructures(String.raw `"abc\"def"`), '');
-  t.equal(prettyRepl._stripCompleteJSStructures(String.raw `"abc\\"def"`), 'def"');
-  t.equal(prettyRepl._stripCompleteJSStructures(String.raw `"a\\\\bc\\"def"`), 'def"');
-  t.equal(prettyRepl._stripCompleteJSStructures('(function {}'), '(function ');
-  t.equal(prettyRepl._stripCompleteJSStructures('(function() {'), '(function() {');
-  t.equal(prettyRepl._stripCompleteJSStructures('(function() => {'), '(function() => {');
-  t.equal(prettyRepl._stripCompleteJSStructures('(function() => {}'), '(function => ');
-  t.equal(prettyRepl._stripCompleteJSStructures('a.b([{x:{y: {z:[0, 10]}}}, {p:"$x"},{q'), 'a.b([, ,{q');
+  assert.equal(prettyRepl._stripCompleteJSStructures('{a: (x) => x.y = 1}'), '');
+  assert.equal(prettyRepl._stripCompleteJSStructures('{x} `${unfinished'), ' `${unfinished');
+  assert.equal(prettyRepl._stripCompleteJSStructures('{(x}'), '{(x}');
+  assert.equal(prettyRepl._stripCompleteJSStructures(String.raw `"abc\"def"`), '');
+  assert.equal(prettyRepl._stripCompleteJSStructures(String.raw `"abc\\"def"`), 'def"');
+  assert.equal(prettyRepl._stripCompleteJSStructures(String.raw `"a\\\\bc\\"def"`), 'def"');
+  assert.equal(prettyRepl._stripCompleteJSStructures('(function {}'), '(function ');
+  assert.equal(prettyRepl._stripCompleteJSStructures('(function() {'), '(function() {');
+  assert.equal(prettyRepl._stripCompleteJSStructures('(function() => {'), '(function() => {');
+  assert.equal(prettyRepl._stripCompleteJSStructures('(function() => {}'), '(function => ');
+  assert.equal(prettyRepl._stripCompleteJSStructures('a.b([{x:{y: {z:[0, 10]}}}, {p:"$x"},{q'), 'a.b([, ,{q');
 });
 
-test('findMatchingBracket', t => {
-  t.plan(10);
+it('findMatchingBracket', () => {
   const { stdin } = stdio();
   const output = new PassThrough();
   output.isTTY = true;
@@ -136,26 +135,25 @@ test('findMatchingBracket', t => {
     input: stdin,
     output: output
   });
-  t.equal(prettyRepl._findMatchingBracket('abc { def }', 4), 10);
-  t.equal(prettyRepl._findMatchingBracket('abc { def }', 10), 4);
-  t.equal(prettyRepl._findMatchingBracket('abc {( def }', 4), 11);
-  t.equal(prettyRepl._findMatchingBracket('abc {( def }', 5), -1);
-  t.equal(prettyRepl._findMatchingBracket('abc {( def }', 0), -1);
-  t.equal(prettyRepl._findMatchingBracket('abc {( def }', 11), 4);
-  t.equal(prettyRepl._findMatchingBracket('"(")', 0), 2);
-  t.equal(prettyRepl._findMatchingBracket('"(")', 1), -1);
-  t.equal(prettyRepl._findMatchingBracket('`${foo}`', 1), 6);
-  t.equal(prettyRepl._findMatchingBracket('(`${")"}`', 0), -1);
+  assert.equal(prettyRepl._findMatchingBracket('abc { def }', 4), 10);
+  assert.equal(prettyRepl._findMatchingBracket('abc { def }', 10), 4);
+  assert.equal(prettyRepl._findMatchingBracket('abc {( def }', 4), 11);
+  assert.equal(prettyRepl._findMatchingBracket('abc {( def }', 5), -1);
+  assert.equal(prettyRepl._findMatchingBracket('abc {( def }', 0), -1);
+  assert.equal(prettyRepl._findMatchingBracket('abc {( def }', 11), 4);
+  assert.equal(prettyRepl._findMatchingBracket('"(")', 0), 2);
+  assert.equal(prettyRepl._findMatchingBracket('"(")', 1), -1);
+  assert.equal(prettyRepl._findMatchingBracket('`${foo}`', 1), 6);
+  assert.equal(prettyRepl._findMatchingBracket('(`${")"}`', 0), -1);
 });
 
-test('full pass-through test', t => {
-  t.plan(1);
+it('full pass-through test', (done) => {
   process.stdout.isTTY = undefined;
   const input = new PassThrough();
   const output = new PassThrough();
   output.isTTY = true;
   output.getColorDepth = () => 8;
-  const prettyRepl = repl.start({
+  repl.start({
     prompt: 'test-prompt > ',
     input: input,
     output: output,
@@ -165,8 +163,9 @@ test('full pass-through test', t => {
   output.setEncoding('utf8').on('data', data => {
     out += data;
     if (out.endsWith('\n') && !out.startsWith('<done>')) {
-      t.equal(out, '\x1b[1G\x1b[0Jtest-prompt > \x1b[15Gle\b\b\x1b[36mlet\x1b[39m foo = \x1b[33m1\x1b[39m\x1b[33m2\x1b[39m\r\n');
+      assert.equal(out, '\x1b[1G\x1b[0Jtest-prompt > \x1b[15Gle\b\b\x1b[36mlet\x1b[39m foo = \x1b[33m1\x1b[39m\x1b[33m2\x1b[39m\r\n');
       out = '<done>';
+      done();
     }
   });
   input.write('let foo = 12\n');
